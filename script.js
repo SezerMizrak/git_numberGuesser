22429df9d40408d56a1cdb9f25064a97aa1d983c
SezerMizrak <sezermizrak@icloud.com>
2023-12-05 21:58:31 +0300

    tried to create a random text generator

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const singsofZodiac = {
   favoriteWords: ["When","Basically","Interesting","Seriously","What","Okay","Maybe","Really","Obviously","Right","No","Whatever"],
   todo: ["Do That","Do Not Do That","Maybe Do That","Maybe dont do that","Definitely do it"]
 }
+const randomStrings = () =>{
+  for (let i = 0; i<singsofZodiac.length; i++){
+    switch(param) {
+      case sings:
+        console.log(`Your zodiac sign is ${sings[Math.floor(Math.random())*sings.length]}`);
+        break;
+      case favoriteWords:
+        console.log(`Your favorite word is ${favoriteWords[Math.floor(Math.random())*favoriteWords.length]}`);
+        break;
+      case todo:
+        console.log(`I think you  ${todo[Math.floor(Math.random())*todo.length]}`);
+        break;
+      default:
+        break;
+    }
+  }
+}
