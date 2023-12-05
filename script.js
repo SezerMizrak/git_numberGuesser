4ef15135b5b0c3acec5fc88516cbd2694fdcd824
SezerMizrak <sezermizrak@icloud.com>
2023-12-05 21:43:18 +0300

    created arrays and random selector

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-// first
+// first lets create a random selector so when we create arrays with random string
+// it will select randomly everytime
 
 const randomSelector = Math.floor(Math.random());
+
+// lets create our random arrays 
+const singsofZodiac = {
+  sings: ["Aries", "Taurus", "Gemini","Cancer","Leo","Virgo","Libra","Scorpius","Sagittarius","Capricornus","Aquarius","Pisces"],
+  favoriteWords: ["When","Basically","Interesting","Seriously","What","Okay","Maybe","Really","Obviously","Right","No","Whatever"],
+  todo: ["Do That","Do Not Do That","Maybe Do That","Maybe dont do that","Definitely do it"]
+}
